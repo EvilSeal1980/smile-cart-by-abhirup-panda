b709bc97a668e7282eaba9da3abbd692af747d76
EvilSeal1980 <evilseal09@gmail.com>
2024-09-14 09:58:01 +0530

    useState bug fix

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -2,20 +2,40 @@ import { useState, useEffect } from "react";
 
 import productsApi from "apis/products";
 import { Header, PageLoader } from "components/commons";
+import useDebounce from "hooks/useDebounce";
 import { Search } from "neetoicons";
 import { Input, NoData } from "neetoui";
 import { isEmpty } from "ramda";
 
 import ProductListItem from "./ProductListItem";
 
+/*
+Making an API request for every keystroke can overload the
+server and slow down the application. Additionally, another
+issue arises when multiple API requests are triggered one
+after the other with each keystroke. If the first request
+takes longer to process and is served after the second one,
+the user might end up interacting with inaccurate or outdated data.
+
+To fix this issue we will use a debouncing. Debouncing is a
+technique where frequent execution of a function is prevented
+by delaying it for some time. All invocations except the last
+one will be ignored.
+
+In our case, we can use debouncing to ensure that the search
+request to a server is only sent after a certain time interval
+has passed after the user stops typing. This prevents sending
+a request for each keystroke.
+*/
 const ProductList = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [products, setProducts] = useState([]);
   const [searchKey, setSearchKey] = useState("");
+  const debouncedSearchKey = useDebounce(searchKey);
 
   const fetchProducts = async () => {
     try {
-      const data = await productsApi.fetch({ searchTerm: searchKey });
+      const data = await productsApi.fetch({ searchTerm: debouncedSearchKey });
       setProducts(data.products);
     } catch (error) {
       console.log("An error occurred:", error);
@@ -26,7 +46,7 @@ const ProductList = () => {
 
   useEffect(() => {
     fetchProducts();
-  }, [searchKey]);
+  }, [debouncedSearchKey]);
 
   if (isLoading) {
     return <PageLoader />;
